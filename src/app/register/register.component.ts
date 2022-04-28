f6aaf0de46cea2e6efc72f9b81165b235f8d8b33
Bijily <bijily.rajan@teranet.ca>
2022-04-28 11:42:32 +0530

    https://github.com/BijilyRajan/QuizRegister.git

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { QuizService } from '../quiz.service';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  errorMsg: string="";
+  Name:string = "";
+  Email:string = "";
+  hide:boolean=false;
+
+  constructor(private quizService:QuizService, private route : Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+
+    this.quizService.insertParticipant(this.Name, this.Email).subscribe(
+      (data:any) => {
+        // localStorage.clear();
+        // localStorage.setItem('participant', JSON.stringify(data));
+        this.route.navigate(['/quiz'])
+      }
+    )
+  }
+
+}
